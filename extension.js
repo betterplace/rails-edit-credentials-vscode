24613818a0fa951a704ba44612cde295752edb04
Jannosch Müller <janosch.mueller@betterplace.org>
2021-10-05 12:17:16 +0200

    Support filenames like staging.foo.yml.enc

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require('vscode')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,7 +17,7 @@ function activate(context) {
 
 		let fileName = vscode.window.activeTextEditor.document.fileName
 
-		let match = /\/(.[^.\/]+)\.yml\.enc/.exec(fileName)
+		let match = /\/([^\/]+)\.yml\.enc/.exec(fileName)
 		let env = match[1]
 		if (!env) {
 			vscode.window.showInformationMessage(`Error: Can only open files that end with .yml.enc`)
@@ -28,11 +28,11 @@ function activate(context) {
 
 		let terminal = vscode.window.createTerminal('credentials-terminal')
 		terminal.sendText(`env EDITOR="code --new-window --wait" rails credentials:edit --environment ${env}`)
-	});
+	})
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable)
 }
-exports.activate = activate;
+exports.activate = activate
 
 // this method is called when your extension is deactivated
 function deactivate() {}
